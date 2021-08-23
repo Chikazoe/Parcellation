8eefbd20e98798d8fa59db8205f2f5c118bb6a60
nissy-shota <shota.nishiyama44@gmail.com>
2021-08-23 20:28:27 +0900

    add CIFTI GRADIENT

---
+++ code/preproc/cifti_gradient.sh
@@ -0,0 +1,5 @@
+wb_command -cifti-gradient \
+	../../data/Preproc/target_run1_all.dscalar.nii COLUMN \
+	../../results/cifti_gradient/gradient_3000cap.dscalar.nii \
+	-left-surface ../../data/SurfFiles/S1200.L.midthickness_MSMAll.32k_fs_LR.surf.gii \
+    -right-surface ../../data/SurfFiles//S1200.R.midthickness_MSMAll.32k_fs_LR.surf.gii \
