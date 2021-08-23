2536a280357229a0e610c59ddc3ef86d928a878e
nissy-shota <shota.nishiyama44@gmail.com>
2021-08-23 20:38:09 +0900

    CIFTI SMOOTHING

---
+++ code/preproc/smooth.sh
@@ -0,0 +1,8 @@
+wb_command -cifti-smoothing \
+    ../../results/cifti_gradient/gradient_3000cap.dscalar.nii \
+    2.55 \
+    0 \
+    COLUMN \
+	../../results/smooth/gradient_smooth_3000cap.dscalar.nii \
+	-left-surface ../../data/SurfFiles/S1200.L.midthickness_MSMAll.32k_fs_LR.surf.gii \
+    -right-surface ../../data/SurfFiles//S1200.R.midthickness_MSMAll.32k_fs_LR.surf.gii \
